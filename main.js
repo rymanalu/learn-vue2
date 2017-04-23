f389f9f6aef6e9fdba02050b89ad057e7926c978
Roni Yusuf Manalu <rymanalu@gmail.com>
2017-04-23 13:58:33 +0700

    Eps 8

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks">{{ task.task }}</task>
+        </div>
+    `,
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to the store', completed: true },
+                { task: 'Go to the email', completed: false },
+                { task: 'Go to the farm', completed: true },
+                { task: 'Go to work', completed: false }
+            ]
+        };
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
