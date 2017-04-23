35337a1dca89fc7b1b48872aa271777b5483ea78
Roni Yusuf Manalu <rymanalu@gmail.com>
2017-04-23 14:55:27 +0700

    Eps 10

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-Vue.component('message', {
-    props: ['title', 'body'],
-    data() {
-        return {
-            isVisible: true
-        };
-    },
+Vue.component('modal', {
     template: `
-        <article class="message" v-show="isVisible">
-            <div class="message-header">
-                <p>{{ title }}</p>
-                <button class="delete" @click="isVisible = false"></button>
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">
+                    <slot></slot>
+                </div>
             </div>
-            <div class="message-body">
-                {{ body }}
-            </div>
-        </article>
+            <button class="modal-close" @click="$emit('close')"></button>
+        </div>
     `
 });
 
 new Vue({
-    el: '#root'
+    el: '#root',
+    data: {
+        showModal: false
+    }
 });
