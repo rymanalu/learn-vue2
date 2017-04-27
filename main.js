2af54d635894cd6f14a53b7f5dbc9d36805c73cd
Roni Yusuf Manalu <rymanalu@gmail.com>
2017-04-28 00:53:57 +0700

    Eps 14

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-window.Event = new class {
-    constructor() {
-        this.vue = new Vue();
-    }
-
-    fire(event, data = null) {
-        this.vue.$emit(event, data);
-    }
-
-    listen(event, callback) {
-        this.vue.$on(event, callback);
-    }
-};
-
-Vue.component('coupon', {
-    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
-    methods: {
-        onCouponApplied() {
-            Event.fire('applied');
-        }
-    }
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">
+                        <slot name="header"></slot>
+                    </p>
+                    <button class="delete"></button>
+                </header>
+                <section class="modal-card-body">
+                    <slot></slot>
+                </section>
+                <footer class="modal-card-foot">
+                    <slot name="footer">
+                        <a class="button is-success">Okay</a>
+                    </slot>
+                </footer>
+            </div>
+        </div>
+    `
 });
 
 new Vue({
-    el: '#root',
-    data: {
-        couponApplied: false
-    },
-    created() {
-        Event.listen('applied', () => alert('handling it'));
-    }
+    el: '#root'
 });
