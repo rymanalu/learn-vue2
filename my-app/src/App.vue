f275e9baa68121a06cb3f059f28007acaff5c5ea
Roni Yusuf Manalu <rymanalu@gmail.com>
2017-05-03 23:27:32 +0700

    Eps 16

---
+++ my-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <message>Hello There</message>
+    <message>Hello There Again</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue'
+
+export default {
+  name: 'app',
+  components: { Message },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
